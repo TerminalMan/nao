70987246070eed09a473520f08c7752650b01723
gRastello <gabriele.rastello@edu.unito.it>
2019-01-05 14:05:20 +0100

    implemented a getkey function

--- nao.go
+++ nao.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strconv"
 	"time"
 )
@@ -15,6 +16,22 @@ type Flashcard struct {
 	duedate     int
 	repetitions int
 	interval    int
+}
+
+// read a single character from stin without a need for the enter key
+func getkey(prompt string) []byte {
+	// block terminal buffering
+	exec.Command("stty", "-F", "/dev/tty", "cbreak").Run()
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+
+	// get the character
+	fmt.Printf("%s", prompt)
+	var c []byte = make([]byte, 1)
+	os.Stdin.Read(c)
+
+	// reset terminal properties and return
+	exec.Command("stty", "-F", "/dev/tty", "sane").Run()
+	return c
 }
 
 // get then n-th flashcard of the given deck
